2458fbca135b4e0902c9469792e568259d2430b4
d4rkn3ssr3igns <d4rkn3ssr3igns@gmail.com>
2025-01-02 20:05:45 +0000

    feat: Use consts in defineConfig
    Use `SITE.BASE` to define base property.
    Use `SITE.URL` to define site property.

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,10 @@
 // @ts-check
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-import { IMPORTS } from './src/consts'
 import { fileURLToPath } from 'node:url'
 import AutoImport from 'astro-auto-import'
 import { defineConfig } from 'astro/config'
+import { IMPORTS, SITE } from './src/consts'
 import { dirname, resolve } from 'node:path'
 import { transformerColorizedBrackets } from '@shikijs/colorized-brackets'
 
@@ -12,7 +12,8 @@ const dir = dirname(fileURLToPath(import.meta.url))
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+  base: SITE.BASE,
+	site: SITE.URL,
 	integrations: [
     AutoImport({ imports: IMPORTS }),
     sitemap(),
